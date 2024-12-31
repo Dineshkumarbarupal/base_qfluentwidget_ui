5e9ae82bbd52a8eca2f0ac7987f9d48e7878e175
vihaan barupal <159883311+Dineshkumarbarupal@users.noreply.github.com>
2024-12-31 10:17:55 +0530

    new changes

--- practice/automation_practice.py
+++ practice/automation_practice.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 user = "Manoj"
 driver = webdriver.Chrome()
 #driver = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.maximize_window()
-driver.find_element_by_css_selector("#name").send_keys(user)
-driver.find_element_by_id("alertbtn").click()
+driver.find_element((By.CSS_SELECTOR,"#name")).send_keys(user)
+driver.find_element((By.ID,"alertbtn")).click()
 alert = driver.switch_to.alert
 alertText = alert.text
 alert.accept()
